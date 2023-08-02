a6cfac6fa80592a387fd692dcfe8027a503a78c6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-02 16:44:06 -0700

    Override form error submission

--- script.js
+++ script.js
@@ -24,17 +24,18 @@ function addBook(book){
     myLibrary.push(book)
 }
 
-
-
 let book1 = new Book(`The Bible`, `Jesus`, `no`);
 let book2 = new Book(`The Lord of the Rings`, `J.R.R. Tolkein`, `no`);
 let book3 = new Book(`Harry Potter`, `J.K. Rowling`, `yes`);
 
-
-
-
 console.log(book1);
 console.log(myLibrary);
-
 addBook(book1);
 console.log(myLibrary);
+
+
+addBookForm.addEventListener("submit", function(){
+    addBookForm.style.display = "none";
+    newBookButton.style.display = "block";
+    event.preventDefault();
+})
