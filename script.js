1edc0e1398403b9323e05944a0bd71f77c43565b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-03 12:50:14 -0700

    Remove unneccesary message

--- script.js
+++ script.js
@@ -103,21 +103,3 @@ array.forEach((item) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-// let book1 = new Book(`The Bible`, `Jesus`, `no`);
-// let book2 = new Book(`The Lord of the Rings`, `J.R.R. Tolkein`, `no`);
-// let book3 = new Book(`Harry Potter`, `J.K. Rowling`, `yes`);
-
-// console.log(book1);
-// console.log(myLibrary);
-// addBook(book1);
-// console.log(myLibrary);
