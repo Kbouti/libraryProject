7986f93c1b728bce2b2b61b8d7b435f90e8006b9
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-03 11:20:10 -0700

    Use code from chatgpt to publish books to page

--- script.js
+++ script.js
@@ -32,18 +32,13 @@ function addBook(){
         console.log(`you didn't read the book`);
         read = "no" ;
     }
-
-
     let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
-    console.log(newBook.title);
     myLibrary.push(newBook);
-    console.log(myLibrary);
-
     addBookForm.reset();
-}
 
-console.log(`hey man`)
+
+    createTable(myLibrary);
+}
 
 addBookForm.addEventListener("submit", function(){
     addBookForm.style.display = "none";
@@ -52,6 +47,35 @@ addBookForm.addEventListener("submit", function(){
     addBook();
 })
 
+function createTable(array){
+    const table = document.createElement("table");
+
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+
+const headerRow = document.createElement("tr");
+for (const key in array[0]) {
+  const th = document.createElement("th");
+  th.textContent = key;
+  headerRow.appendChild(th);
+
+}
+thead.appendChild(headerRow);
+array.forEach((item) => {
+    const dataRow = document.createElement("tr");
+    for (const key in item) {
+      const td = document.createElement("td");
+      td.textContent = item[key];
+      dataRow.appendChild(td);
+    }
+    tbody.appendChild(dataRow);
+  });
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  booksDiv.appendChild(table);
+}
 
 //   https://www.youtube.com/watch?v=9YGgC1WPpf4&t=880s
 
