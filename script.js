325c9d2e9ebb21887a8967a4e00fbb401c14a45f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-04 11:03:04 -0700

    Remove chatGPT code

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function addBook(){
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     addBookForm.reset();
-    createTable(myLibrary);
+    // createTable(myLibrary);
 }
 
 addBookForm.addEventListener("submit", function(){
@@ -53,40 +53,41 @@ function capitalize(string){
 }
 
 
-function createTable(array){
-    const table = document.createElement("table");
 
-const thead = document.createElement("thead");
-const tbody = document.createElement("tbody");
+// The following code made with help of chatgpt makes a table, but I couldn't figure out how to get buttons in it.
 
-const headerRow = document.createElement("tr");
-for (const key in array[0]) {
-  const th = document.createElement("th");
-let tempString = key;
-let capString = capitalize(tempString);
-  th.textContent = capString;
-  headerRow.appendChild(th);
-}
-thead.appendChild(headerRow);
+// function createTable(array){
+//     const table = document.createElement("table");
 
+// const thead = document.createElement("thead");
+// const tbody = document.createElement("tbody");
 
-array.forEach((item) => {
-    const dataRow = document.createElement("tr");
-    for (const key in item) {
-      const td = document.createElement("td");
-      td.textContent = item[key];
-      dataRow.appendChild(td);
-    }
-    tbody.appendChild(dataRow);
-  });
+// const headerRow = document.createElement("tr");
+// for (const key in array[0]) {
+//   const th = document.createElement("th");
+// let tempString = key;
+// let capString = capitalize(tempString);
+//   th.textContent = capString;
+//   headerRow.appendChild(th);
+// }
+// thead.appendChild(headerRow);
+// array.forEach((item) => {
+//     const dataRow = document.createElement("tr");
+//     for (const key in item) {
+//       const td = document.createElement("td");
+//       td.textContent = item[key];
+//       dataRow.appendChild(td);
+//     }
+//     tbody.appendChild(dataRow);
+//   });
 
-  table.appendChild(thead);
-  table.appendChild(tbody);
+//   table.appendChild(thead);
+//   table.appendChild(tbody);
 
-  booksDiv.innerHTML = "";
-  booksDiv.appendChild(table);
+//   booksDiv.innerHTML = "";
+//   booksDiv.appendChild(table);
 
-}
+// }
 
 //   https://www.youtube.com/watch?v=9YGgC1WPpf4&t=880s
 
